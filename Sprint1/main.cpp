6c04b6c859107ba80e0de026c3975f8c0c531487
pattisonb <pattisonb@github.com>
2019-09-10 17:12:43 -0500

    changed file inputting

--- Sprint1/main.cpp
+++ Sprint1/main.cpp
@@ -12,18 +12,20 @@ Team* createTeam(ifstream& teamData);
 
 int main(int argc, char** argv)
 {
-    ifstream team1Data(argv[0]);
-    ifstream team2Data(argv[1]);
-    ifstream matchData(argv[2]);
-    ofstream outputFile(argv[3]);
+    //ifstream team1Data(argv[0]);
+    ifstream team1Data("cowboys.txt");
+    //ifstream team2Data(argv[1]);
+    //ifstream matchData(argv[2]);
+    //ofstream outputFile(argv[3]);
 
     Team* team1 = createTeam(team1Data);
-    Team* team2 = createTeam(team2Data);
+    //Team* team2 = createTeam(team2Data);
 }
 
 Team* createTeam(ifstream& teamData) {
     string buffer;
     getline(teamData, buffer); //getting the team name into buffer string
+    cout << buffer << endl;
     DSString teamName(buffer);
     int teamSize;
     teamData >> teamSize;
